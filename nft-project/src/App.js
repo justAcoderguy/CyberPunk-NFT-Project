b02f28cdebca48a4b2bb04ca5a2d52597b29281a
Abhishek Banerjee <abhishek.banerjee18@outlook.com>
2022-05-11 00:57:52 +0530

    Added Collection Cards

--- nft-project/src/App.js
+++ nft-project/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import CollectionCard from './components/CollectionCard';
 import Header from './components/Header';
 
 function App() {
+
+
+
   return <div className='app'>
     <Header />
+    <CollectionCard 
+      id={0} 
+      name={'Miki Punk'} 
+      traits={[{'value': 7}]} 
+      image='https://ipfs.thirdweb.com/ipfs/QmZ5fD3UTRh8ALZCpMdypHkhMQSXyi4yyCz3Ea19kPmtXg/0.jpg'
+    />
   </div> 
 }
 
